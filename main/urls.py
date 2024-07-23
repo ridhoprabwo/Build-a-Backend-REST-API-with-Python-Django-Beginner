aaa9223cc12b85c5e3510ccf784494a4cfb670f2
Ridho Prabowo <ridhoprabwo@gmail.com>
2024-07-23 15:01:12 +0700

    Added HelloViewSet to API

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from main.app.views import HelloAPIView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from main.app.views import HelloAPIView, HelloViewSet
+
+router = DefaultRouter()
+router.register(r"tes-viewset", HelloViewSet, basename="viewset")
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/tes/", HelloAPIView.as_view(), name="hello"),
+    path("api/tes-viewapi/", HelloAPIView.as_view(), name="viewapi"),
+    path("api/", include(router.urls)),
 ]
