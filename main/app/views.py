144d9ec1433261577ce00a520c34a3eb4deb5231
Ridho Prabowo <ridhoprabwo@gmail.com>
2024-07-24 11:15:49 +0700

    Added searching for user api

--- main/app/views.py
+++ main/app/views.py
@@ -1,5 +1,5 @@
 # External libraries
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
@@ -115,3 +115,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("name", "email")
